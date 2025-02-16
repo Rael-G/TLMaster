19c574a597babd50fed9d75d0693045d466ce196
Israel Albino Galvan <israel.galvan@live.com>
2025-02-16 16:11:55 -0300

    Feat configure persistance

---
+++ TLMaster/Persistance/PersistanceExtensions.cs
@@ -0,0 +1,23 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using TLMaster.Core.Interfaces.Repositories;
+using TLMaster.Persistance.Contexts;
+using TLMaster.Persistance.Repositories;
+
+namespace TLMaster.Persistance;
+
+public static class PersistanceExtensions
+{
+    public static void ConfigurePersistance(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("ConnectionString")));
+
+        services.AddScoped<IAuctionRepository, AuctionRepository>();
+        services.AddScoped<IBidRepository, BidRepository>();
+        services.AddScoped<ICharacterRepository, CharacterRepository>();
+        services.AddScoped<IGuildRepository, GuildRepository>();
+        services.AddScoped<IItemRepository, ItemRepository>();
+        services.AddScoped<IPartyRepository, PartyRepository>();
+    }
+}
