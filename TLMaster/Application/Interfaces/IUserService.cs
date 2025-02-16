098cacde8a74a49b9225893161b3d9c5425c9ef6
Israel Albino Galvan <israel.galvan@live.com>
2025-02-16 18:08:43 -0300

    Feat user service

--- TLMaster/Application/Interfaces/IUserService.cs
+++ TLMaster/Application/Interfaces/IUserService.cs
@@ -1,4 +1,5 @@
 using System;
+using Microsoft.AspNetCore.Identity;
 
 namespace TLMaster.Application.Interfaces;
 
@@ -8,27 +9,29 @@ public interface IUserService
     /// Creates a new user.
     /// </summary>
     /// <param name="user">The user to create.</param>
-    Task Create(UserDto user);
+    /// <returns>The identity result</returns>
+    Task<IdentityResult> Create(UserDto user, string password);
 
     /// <summary>
     /// Updates an existing user.
     /// </summary>
     /// <param name="user">The user to update.</param>
-    Task Update(UserDto user);
+    /// <returns>The identity result</returns>
+    Task<IdentityResult> Update(UserDto user);
 
     /// <summary>
     /// Deletes an existing user.
     /// </summary>
     /// <param name="user">The user to delete.</param>
-    /// <returns>True if the user was deleted, False if not.</returns>
-    Task<bool> Delete(UserDto user);
+    /// <returns>The identity result</returns>
+    Task<IdentityResult> Delete(UserDto user);
 
     /// <summary>
     /// Retrieves a user by its unique identifier.
     /// </summary>
     /// <param name="id">The unique identifier of the user to retrieve.</param>
     /// <returns>The retrieved user, or null if not found.</returns>
-    Task<UserDto?> Get(Guid id);
+    Task<UserDto?> GetById(Guid id);
 
     /// <summary>
     /// Retrieves all entitys.
---
+++ TLMaster/Application/Services/UserService.cs
@@ -0,0 +1,54 @@
+using System;
+using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using TLMaster.Application.Interfaces;
+using TLMaster.Core.Entities;
+
+namespace TLMaster.Application.Services;
+
+public class UserService(UserManager<User> userManager, IMapper mapper)
+    : IUserService
+{
+    private readonly UserManager<User> _userManager = userManager;
+    private readonly IMapper _mapper = mapper;
+
+    public async Task<IdentityResult> Create(UserDto userDto, string password)
+    {
+        var user = _mapper.Map<User>(userDto);
+        return await _userManager.CreateAsync(user, password);
+    }
+
+    public async Task<IdentityResult> Update(UserDto userDto)
+    {
+        var user = await _userManager.FindByIdAsync(userDto.Id.ToString());
+        if (user is null) return IdentityResult.Failed(new IdentityError { Description = "User not found" });
+
+        _mapper.Map(userDto, user);
+        return await _userManager.UpdateAsync(user);
+    }
+
+    public async Task<IdentityResult> Delete(UserDto userDto)
+    {
+        var user = await _userManager.FindByIdAsync(userDto.Id.ToString());
+        return user is not null ? await _userManager.DeleteAsync(user) : IdentityResult.Failed(new IdentityError { Description = "User not found" });
+    }
+
+        public async Task<UserDto?> GetById(Guid id)
+    {
+        var user = await _userManager.FindByIdAsync(id.ToString());
+        return user is not null ? _mapper.Map<UserDto>(user) : null;
+    }
+
+    public async Task<UserDto?> GetByUsername(string username)
+    {
+        var user = await _userManager.FindByNameAsync(username);
+        return user is not null ? _mapper.Map<UserDto>(user) : null;
+    }
+
+    public Task<IEnumerable<UserDto>> GetAll()
+    {
+        var users = _userManager.Users.ToList();
+        return Task.FromResult(_mapper.Map<IEnumerable<UserDto>>(users));
+    }
+}
+
