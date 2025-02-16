6a5d1cda6e2f396eebdad43d7c39ede13d1345fc
Israel Albino Galvan <israel.galvan@live.com>
2025-02-16 15:48:33 -0300

    Feat BaseRepository

---
+++ TLMaster/Core/Interfaces/Repositories/IBaseRepository.cs
@@ -0,0 +1,44 @@
+using System;
+using TLMaster.Core.Entities;
+
+namespace TLMaster.Core.Interfaces.Repositories;
+
+
+public interface IBaseRepository<T> where T : BaseEntity
+{
+    /// <summary>
+    /// Creates a new entity.
+    /// </summary>
+    /// <param name="entity">The entity to create.</param>
+    void Create(T entity);
+
+    /// <summary>
+    /// Updates an existing entity.
+    /// </summary>
+    /// <param name="entity">The entity to update.</param>
+    void Update(T entity);
+
+    /// <summary>
+    /// Deletes an existing entity.
+    /// </summary>
+    /// <param name="entity">The entity to delete.</param>
+    void Delete(T entity);
+
+    /// <summary>
+    /// Retrieves an entity by its unique identifier.
+    /// </summary>
+    /// <param name="id">The unique identifier of the entity to retrieve.</param>
+    /// <returns>The retrieved entity, or null if not found.</returns>
+    Task<T?> Get(Guid id);
+
+    /// <summary>
+    /// Retrieves all entities of type T.
+    /// </summary>
+    /// <returns>A collection of entities of type T.</returns>
+    Task<IEnumerable<T>> GetAll();
+
+    /// <summary>
+    /// Commits any pending changes to the underlying data store.
+    /// </summary>
+    Task Commit();
+}
---
+++ TLMaster/Persistance/Repositories/BaseRepository.cs
@@ -0,0 +1,36 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using TLMaster.Core.Entities;
+using TLMaster.Core.Interfaces.Repositories;
+using TLMaster.Persistance.Context;
+
+namespace TLMaster.Persistance.Repositories;
+
+
+public abstract class BaseRepository<T>(ApplicationDbContext context)
+    : IBaseRepository<T> where T : BaseEntity
+{
+    protected readonly ApplicationDbContext Context = context;
+
+    public virtual void Create(T entity)
+        => Context.Add(entity);
+
+    public virtual void Update(T entity)
+        => Context.Update(entity);
+
+    public virtual void Delete(T entity)
+        => Context.Remove(entity);
+
+    public virtual async Task<T?> Get(Guid id)
+        => await Context.Set<T>()
+        .AsNoTracking()
+        .FirstOrDefaultAsync(t => t.Id == id);
+
+    public virtual async Task<IEnumerable<T>> GetAll()
+        => await Context.Set<T>()
+        .AsNoTracking()
+        .ToListAsync();
+
+    public async Task Commit()
+        => await Context.SaveChangesAsync();
+}
