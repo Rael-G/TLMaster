b042c52d6ec12705a905edaf57e19556b5e90bed
Israel Albino Galvan <israel.galvan@live.com>
2025-02-21 03:13:05 -0300

    Feat forbidden access exception

---
+++ TLMaster/Application/Exceptions/ForbiddenAccessException.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace TLMaster.Application.Exceptions;
+
+public class ForbiddenAccessException : Exception
+{
+    public ForbiddenAccessException()
+    {
+        
+    }
+
+    public ForbiddenAccessException(string? message) 
+        : base(message)
+    {
+        
+    }
+
+    public ForbiddenAccessException(string? message, Exception? innerException) 
+        : base(message, innerException)
+    {
+        
+    }
+}
