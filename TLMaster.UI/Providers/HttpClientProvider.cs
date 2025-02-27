ee5e568ca95f4725c978fefaeeb0f29509d782f8
Israel Albino Galvan <israel.galvan@live.com>
2025-02-27 15:36:07 -0300

    Fix possible circular dependency on http client provider

--- TLMaster.UI/Providers/HttpClientProvider.cs
+++ TLMaster.UI/Providers/HttpClientProvider.cs
@@ -1,18 +1,16 @@
-using System;
 using System.Net.Http.Headers;
-using TLMaster.UI.Services;
+using Blazored.LocalStorage;
 
 namespace TLMaster.UI.Providers;
 
-public class HttpClientProvider(HttpClient httpClient, AuthService authService)
+public class HttpClientProvider(HttpClient httpClient, ILocalStorageService localStorage)
 {
     private readonly HttpClient _httpClient = httpClient;
-    private readonly AuthService _authService = authService;
-
+    private readonly ILocalStorageService _localStorage = localStorage;
 
     public async Task<HttpClient> GetClient() 
     {
-        var token = await _authService.GetAccessToken();
+        var token = await _localStorage.GetItemAsync<string>("accessToken");
         
         if (!string.IsNullOrEmpty(token))
         {
