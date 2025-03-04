efd86ffc2d320c30f93ce9f71b35fea3bb864749
Israel Albino Galvan <israel.galvan@live.com>
2025-03-04 15:17:09 -0300

    Refactor GuildService to inherit BaseService

---
+++ TLMaster.UI/Services/BaseService.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Net.Http.Json;
+using TLMaster.UI.Providers;
+
+namespace TLMaster.UI.Services;
+
+public abstract class BaseService<TDto, TInput>(HttpClientProvider httpClientProvider)
+{
+    protected readonly HttpClient HttpClient = httpClientProvider.GetAuthenticatedClient();
+    protected abstract string Endpoint { get; set;}
+
+    public virtual async Task<List<TDto>> GetAllAsync()
+    {
+        return await HttpClient.GetFromJsonAsync<List<TDto>>(Endpoint) 
+            ?? [];
+    }
+
+    public virtual async Task<TDto?> GetByIdAsync(Guid id)
+    {
+        return await HttpClient.GetFromJsonAsync<TDto>($"{Endpoint}/{id}");
+    }
+
+    public virtual async Task<bool> CreateAsync(TInput input)
+    {
+        var response = await HttpClient.PostAsJsonAsync(Endpoint, input);
+        return response.IsSuccessStatusCode;
+    }
+
+    public virtual async Task<bool> UpdateAsync(Guid id, TInput input)
+    {
+        var response = await HttpClient.PutAsJsonAsync($"{Endpoint}/{id}", input);
+        return response.IsSuccessStatusCode;
+    }
+
+    public virtual async Task<bool> DeleteAsync(Guid id)
+    {
+        var response = await HttpClient.DeleteAsync($"{Endpoint}/{id}");
+        return response.IsSuccessStatusCode;
+    }
+}
--- TLMaster.UI/Services/GuildService.cs
+++ TLMaster.UI/Services/GuildService.cs
@@ -5,44 +5,7 @@ using TLMaster.UI.Providers;
 
 namespace TLMaster.UI.Services;
 
-public class GuildService
+public class GuildService(HttpClientProvider httpClientProvider) : BaseService<GuildDto, GuildInputModel>(httpClientProvider)
 {
-    private readonly HttpClientProvider _httpClientProvider;
-    private readonly HttpClient _httpClient;
-    private const string Endpoint = "api/guild";
-
-    public GuildService(HttpClientProvider httpClientProvider)
-    {
-        _httpClientProvider = httpClientProvider;
-        _httpClient = _httpClientProvider.GetAuthenticatedClient();
-    }
-
-    public async Task<List<GuildDto>> GetAllAsync()
-    {
-        return await _httpClient.GetFromJsonAsync<List<GuildDto>>(Endpoint) 
-            ?? [];
-    }
-
-    public async Task<GuildDto?> GetByIdAsync(Guid id)
-    {
-        return await _httpClient.GetFromJsonAsync<GuildDto>($"{Endpoint}/{id}");
-    }
-
-    public async Task<bool> CreateAsync(GuildInputModel input)
-    {
-        var response = await _httpClient.PostAsJsonAsync(Endpoint, input);
-        return response.IsSuccessStatusCode;
-    }
-
-    public async Task<bool> UpdateAsync(Guid id, GuildInputModel input)
-    {
-        var response = await _httpClient.PutAsJsonAsync($"{Endpoint}/{id}", input);
-        return response.IsSuccessStatusCode;
-    }
-
-    public async Task<bool> DeleteAsync(Guid id)
-    {
-        var response = await _httpClient.DeleteAsync($"{Endpoint}/{id}");
-        return response.IsSuccessStatusCode;
-    }
+    protected override string Endpoint { get; set; } = "api/guild";
 }
