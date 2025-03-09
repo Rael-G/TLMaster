f904aee68f01ae2e01e2a46d18f846881fb1eabe
Israel Albino Galvan <israel.galvan@live.com>
2025-03-09 00:37:53 -0300

    Fix guild summary guildmaster

--- TLMaster/Application/Dtos/Summaries/GuildSummaryDto.cs
+++ TLMaster/Application/Dtos/Summaries/GuildSummaryDto.cs
@@ -7,5 +7,5 @@ public class GuildSummaryDto
     public Guid Id { get; set; } 
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    public CharacterSummaryDto GuildMaster { get; set; } = new();
+    public UserSummaryDto GuildMaster { get; set; } = new();
 }
